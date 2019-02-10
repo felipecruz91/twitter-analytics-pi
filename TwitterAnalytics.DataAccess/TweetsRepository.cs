4c6d32a0fb98c93083ade9589d13e06e756984ae
Felipe Cruz <fcz@kamstrup.com>
2019-02-10 13:08:12 +0100

    Created DAL Unit tests

--- TwitterAnalytics.DataAccess/TweetsRepository.cs
+++ TwitterAnalytics.DataAccess/TweetsRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using InfluxDB.Collector;
-using InfluxDB.Collector.Diagnostics;
+﻿using System.Collections.Generic;
 using Tweetinvi.Events;
 using TwitterAnalytics.BusinessLogic;
 
@@ -9,49 +6,43 @@ namespace TwitterAnalytics.DataAccess
 {
     public class TweetsRepository : ITweetsRepository
     {
-        public MetricsCollector Collector;
+        private readonly IMetricsCollectorWrapper _metricsCollectorWrapper;
 
-        public TweetsRepository(string serverBaseAddress, string database, TimeSpan interval)
+        public TweetsRepository(IMetricsCollectorWrapper metricsCollectorWrapper)
         {
-            Console.WriteLine($"[{DateTime.Now}] - Influx DB server address: '{serverBaseAddress}'.");
-
-            Collector = new CollectorConfiguration()
-                .Batch.AtInterval(interval)
-                .WriteTo.InfluxDB(serverBaseAddress, database)
-                .CreateCollector();
-
-            CollectorLog.RegisterErrorHandler((message, exception) =>
-            {
-                Console.Error.WriteLine($"[{DateTime.Now}] - {message}: {exception}");
-            });
+            _metricsCollectorWrapper = metricsCollectorWrapper;
         }
 
         public void SaveTweet(MatchedTweetReceivedEventArgs args)
         {
-            Collector.Write("tweet",
-                new Dictionary<string, object>
-                {
-                    {"text", args.Tweet.Text},
-                    {"screen_name", args.Tweet.CreatedBy.UserIdentifier.ScreenName},
-                    {"isRetweet", args.Tweet.IsRetweet},
-                    {"retweetCount", args.Tweet.RetweetCount},
-                    {"favorited", args.Tweet.Favorited},
-                    {"favoriteCount", args.Tweet.FavoriteCount},
-                    {"created_at", args.Tweet.CreatedAt}
-                });
+            var fields = new Dictionary<string, object>
+            {
+                {"text", args.Tweet.Text},
+                {"screen_name", args.Tweet.CreatedBy.UserIdentifier.ScreenName},
+                {"isRetweet", args.Tweet.IsRetweet},
+                {"retweetCount", args.Tweet.RetweetCount},
+                {"favorited", args.Tweet.Favorited},
+                {"favoriteCount", args.Tweet.FavoriteCount},
+                {"created_at", args.Tweet.CreatedAt}
+            };
+
+            _metricsCollectorWrapper.Write("tweet", fields);
         }
 
         public void SaveSentiment(TweetSentiment tweetSentiment)
         {
-            if (tweetSentiment != null)
+            if (tweetSentiment == null)
             {
-                Collector.Write("sentiment",
-                    new Dictionary<string, object>
-                    {
-                        {"fullText", tweetSentiment.FullText},
-                        {"score", tweetSentiment.Score}
-                    });
+                return;
             }
+
+            var fields = new Dictionary<string, object>
+            {
+                {"fullText", tweetSentiment.FullText},
+                {"score", tweetSentiment.Score}
+            };
+
+            _metricsCollectorWrapper.Write("sentiment", fields);
         }
     }
 }
